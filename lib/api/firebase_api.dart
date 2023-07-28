db8d59f8af0835d06ffd5f7889c7fae6d523d196
Abbasrx77 <abbasmamadaniel@gmail.com>
2023-07-28 13:06:28 +0100

    Finalisation de la mise en place des notifications

--- lib/api/firebase_api.dart
+++ lib/api/firebase_api.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get/get.dart';
 
@@ -12,10 +16,30 @@ class FirebaseApi{
   final _firebaseMessaging = FirebaseMessaging.instance;
   final storage = const FlutterSecureStorage();
 
-  void handleMessage(RemoteMessage? message){
-    if(message == null) return;
+  final _androidChannel = const AndroidNotificationChannel(
+    'high_importance_channel',
+    'High Importance Notifications',
+    description: 'This channel is used for important notifications',
+    importance: Importance.high,
+  );
+  final _localNotifications = FlutterLocalNotificationsPlugin();
 
-    //Navigation vers la page voulue avec arguments
+  Future initLocalNotifications()async{
+    const android = AndroidInitializationSettings('@drawable/logo_in_app');
+    const settings = InitializationSettings(android: android);
+    
+    await _localNotifications.initialize(
+      settings,
+      onDidReceiveNotificationResponse: (payload){
+        final message = RemoteMessage.fromMap(jsonDecode(payload as String));
+        handleMessage(message);
+      },
+    );
+
+    final platform = _localNotifications.resolvePlatformSpecificImplementation<
+    AndroidFlutterLocalNotificationsPlugin>();
+    await platform?.createNotificationChannel(_androidChannel);
+    
   }
 
   Future initPushNotifications() async{
@@ -25,11 +49,35 @@ class FirebaseApi{
       badge: true,
       sound: true,
     );
-
     FirebaseMessaging.instance.getInitialMessage()
         .then(handleMessage);
     FirebaseMessaging.onMessageOpenedApp.listen(handleMessage);
     FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
+    FirebaseMessaging.onMessage.listen((message) {
+      final notification = message.notification;
+      if(notification == null) return;
+
+      _localNotifications.show(
+          notification.hashCode,
+          notification.title,
+          notification.body,
+          NotificationDetails(
+            android: AndroidNotificationDetails(
+              _androidChannel.id,
+              _androidChannel.name,
+              channelDescription: _androidChannel.description,
+              icon: '@drawable/logo_in_app'
+            ),
+          ),
+        payload: jsonEncode(message.toMap()),
+      );
+    });
+  }
+
+  void handleMessage(RemoteMessage? message){
+    if(message == null) return;
+
+    //Navigation vers la page voulue avec arguments
   }
 
   Future<void> initNotifications() async{
@@ -38,5 +86,6 @@ class FirebaseApi{
     await storage.write(key: 'fcmToken', value: fCMToken);
     print("Token: $fCMToken");
     initPushNotifications();
+    initLocalNotifications();
   }
 }
